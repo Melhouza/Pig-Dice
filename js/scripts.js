b25f800fb9d315a877457093fc0d018e12ab9e66
Melvin Gitau <gitaumel@gmail.com>
2018-08-27 17:51:35 +0300

    changes to index.html

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var randomize = function(){
+  var rolledNumber = Math.floor((Math.random() * 6) + 1);
+  return rolledNumber;
+}
+
+$(document).ready(function() {
+$("button#roll").click(function(){
+
+  var rolledNumber= randomize();
+
+  $("#rollednumber").text(rolledNumber);
+
+
+  if(rolledNumber==1  ) {
+
+    alert("Ooops!!You rolled 1.It's the other player's turn");
+  }
+});
+  $("button#hold").click(function(){
+    alert("Its the other players turn");
+  });
+$("button#roll2").click(function(){
+    var rolledNumberb= randomize();
+
+$("#rolledNumberb").text(rolledNumberb);
+
+  });
+if(rolledNumberb==1) {
+
+  alert("Ooops!!You rolled 1.It's the other player's turn");
+}
+$("button#hold2").click(function(){
+alert("Its the other players turn");
+  });
+});
